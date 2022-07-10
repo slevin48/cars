230a68b6208cd9aab44d42018855717ff69a1e98
Slevin <yann.debray@gadz.org>
2022-07-10 16:09:25 +0200

    Generate multi-entry form from list

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Simply Copy & Paste information into a Form as a Streamlit App
 
-[streamlit-streamlit_app-2022-07-10-14-07-48.webm](https://user-images.githubusercontent.com/12418115/178144295-4ab49e30-b54b-458a-b0d6-0d9f76649570.webm)
+[streamlit-streamlit_app-2022-07-10-16-07-11.webm](https://user-images.githubusercontent.com/12418115/178148295-79539a48-272b-441d-91f9-b330002ac277.webm)
+
+Manage State of the form: 
+- [https://docs.streamlit.io/library/api-reference/session-state](https://docs.streamlit.io/library/api-reference/session-state)
+- [https://docs.streamlit.io/library/advanced-features/session-state](https://docs.streamlit.io/library/advanced-features/session-state)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,35 @@
 
 import streamlit as st
 import clipboard
+import pandas as pd
 
 st.title("Entry Form 📋")
 
-c1, c2 = st.sidebar.columns(2)
+p = ["url","model","price","mileage","options"]
+n = len(p)
+b = [None]*n
+v = [None]*n
+f = [None]*n
 
-with c1:
-    st.text("Text1")
-    b1 = st.button("📋")
-with c2:
-    if b1:
-        v1 = clipboard.paste()
-    else:
-        v1 = ""
-    f1 = st.text_input("",value = v1)
+
+for i in range(n):
+
+    # initialisation
+    if i not in st.session_state:
+        st.session_state[i] = ""
+
+    c1, c2 = st.sidebar.columns(2)
+
+    with c1:
+        st.text(p[i])
+        b[i] = st.button("📋",key="b"+str(i))
+    with c2:
+        if b[i]:
+            v[i] = clipboard.paste()
+            st.session_state[i] = v[i]
+        else:
+            v[i] = st.session_state[i]
+        f[i] = st.text_input("",value = v[i],key="f"+str(i))
+    
+# st.session_state
+st.table(pd.DataFrame.from_dict({p[i]:f[i] for i in range(n)},orient='index'))
